14748977bbbc494550bec15756d7e14c37afc735
ANDK <andriikalinkin@gmail.com>
2023-09-21 19:10:26 +0300

    Add /space route to main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import csv
+
 from flask import Flask
+import requests
 
 
 app = Flask(__name__)
@@ -17,3 +20,39 @@ def requirements() -> str:
     requirements_text = "<br>".join(data)
 
     return requirements_text
+
+
+@app.route("/mean")
+def mean() -> str:  # LibreOffice = 127.079_421_160_8; Python = 127.079_421_160_799_16;
+    total = 0
+    count = 0
+
+    with open("./hw.csv", mode="r") as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        for row in reader:
+            try:
+                total += float(row[2])
+                count += 1
+            except IndexError:
+                pass
+
+    average = total / count
+
+    return str(average)
+
+
+@app.route("/space")
+def space() -> int:
+    try:
+        url = "http://api.open-notify.org/astros.json"
+
+        response = requests.get(url)
+        data = response.json()
+        total_astronauts = data.get("number", 0)
+
+        return str(total_astronauts)
+    except Exception as e:
+        print(e)
+        return 0
